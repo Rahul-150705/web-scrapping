ea59636b7e89721a4e99da5706b8c3ff18444a2c
Rahul-150705 <rahul.cd23@bitsathy.ac.in>
2026-02-06 16:19:52 +0530

    added extra comment

--- web scrapping/selenium/organic_vs_ad.py
+++ web scrapping/selenium/organic_vs_ad.py
@@ -34,17 +34,17 @@ def scrape(p,by,location):
 def scrape2(p,by,location,attribute):
     try:
         return p.find_element(by,location).get_attribute(attribute) # dont add text bcz its a link
-    except NoSuchElementException:
+    except (NoSuchElementException,NoSuchAttributeException):
         return ""
 # lazy loading to load image url and upload it in list
 def ensure_lazy_images_loaded(driver, step=400, delay=0.4):
-    height = driver.execute_script("return document.body.scrollHeight")
+    height = driver.execute_script("return document.body.scrollHeight") # calculate initial height
     y = 0
-
-    while y < height:
-        driver.execute_script(f"window.scrollTo(0, {y});")
-        time.sleep(delay)
-        y += step
+    while y < height: # loop till y is greater
+        driver.execute_script(f"window.scrollTo(0, {y});") # scroll to will scroll till y point
+        time.sleep(delay) # then wait for some second
+        y += step # then increase y
+        height=driver.execute_script("return document.body.scrollHeight") # this will increase if its a infinite scroll page if not it will remain same
 
 # looping to add all scraped element to the list
 def looping(driver):
@@ -68,6 +68,8 @@ def looping(driver):
             else:
                 organic.append(row)
         return sponsor,organic
+
+
 # to check if pagination exist or not
 def next_pagination(driver,product):
     try:
@@ -76,14 +78,15 @@ def next_pagination(driver,product):
             return False
         first_product=product[0]
         driver.execute_script(
-            "arguments[0].scrollIntoView({block:'center'});",next_page )
-        driver.execute_script("arguments[0].click();",next_page)
-        WebDriverWait(driver,10).until(
+            "arguments[0].scrollIntoView({block:'center'});",next_page ) # in this argument[0]-> next_page it will load until that is encountered if encountered then
+        driver.execute_script("arguments[0].click();",next_page) # it will click that next page this is what makes you move to next page
+        WebDriverWait(driver,10).until( # we need remove the first_product from the driver to load next page
             EC.staleness_of(first_product)
         )
         return True
-    except (NoSuchElementException,TimeoutException):
+    except (NoSuchElementException,TimeoutException): # if next is not there or webdriver did not fetch any data then it will return false
         return False
+
 
 #to load into csv
 def to_csv(organic,sponsored):
@@ -108,17 +111,17 @@ def main():
     while page<=2:
         print("Scrapping:",page)
         ensure_lazy_images_loaded(driver, step=400, delay=0.4)
-        product = driver.find_elements(By.CSS_SELECTOR, "li.product-base")
+        product = driver.find_elements(By.CSS_SELECTOR, "li.product-base") # load the product
         sponsored,organic=looping(driver)
         orgainc_data.extend(organic)
         sponsored_data.extend(sponsored)
-        d=next_pagination(driver,product)
-        if not d:
+        next_page_exist=next_pagination(driver,product)
+        if not next_page_exist:
             break
         page+=1
     to_csv(orgainc_data,sponsored_data)
     driver.quit()
-# to call main function 
+# to call main function
 if __name__ == "__main__":
     main()
 
